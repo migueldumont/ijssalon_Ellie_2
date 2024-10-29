356719e8d24e2667c0236e5f6b710520e3c83a5a
Miguel <miguel.dumont@hotmail.com>
2024-10-29 22:18:37 +0100

    Bijkomende functies toegevoegd huiswerk 10

--- helper.py
+++ helper.py
@@ -5,3 +5,18 @@ def decoreer(tekst=""):
     print(f"* {tekst} *")
     print(lengte* "*")
     print()
+
+def fooi_pp(bedrag,personen):
+    bedrag_pp=bedrag/personen
+    return f"Het bedrag per personen is {bedrag_pp} euro."
+
+def onderstreep(tekst=""):
+    uit=[]
+    uit.append(tekst)
+    uit.append(len(tekst)*"=")
+    return uit
+
+def som(data: dict):
+    return sum(data.values())
+
+
---
+++ presentatie.py
@@ -0,0 +1,7 @@
+def presenteer(data:dict,totaal):
+    for k,v in data.items():
+          print(f"{k} : {v} euro")
+
+    print("="*(26))
+    print(f"totaal : {totaal} euro")
+    
